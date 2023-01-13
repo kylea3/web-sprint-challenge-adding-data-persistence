e850f4e126e31d678b0211bdd49dda621766282f
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-13 14:14:18 -0700

    set get and post responses in routers

--- api/project/router.js
+++ api/project/router.js
@@ -2,4 +2,20 @@
 const router = require('express').Router()
 const Project = require('./model')
 
+router.get('/', (req, res, next) => {
+
+})
+
+router.post('/', (req, res, next) => {
+    
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      customMessage: 'The server had an error',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
 module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -2,4 +2,21 @@
 const router = require('express').Router()
 const Task = require('./model')
 
+router.get('/', (req, res, next) => {
+    
+})
+
+router.post('/', (req, res, next) => {
+    
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      customMessage: 'The server had an error',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
+
 module.exports = router;
